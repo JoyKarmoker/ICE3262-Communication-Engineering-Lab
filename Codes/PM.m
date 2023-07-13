9350e3d6e7c2c23593e40f81a3dacc4ab0ffbfd8
JoyKarmoker <Joykarmoker353@gmail.com>
2023-07-13 09:28:57 +0600

    Modified

--- Codes/PM.m
+++ Codes/PM.m
@@ -1,13 +1,11 @@
 clc;
-close all;
 clear all;
+close all;
 
-t = 0:0.001:1;
+am = 1;
 fm = 2;
-fc = 100;
-am = 2;
-ac = 5;
-msg = am*sin(2*pi*fm*t);
+t = 0: 0.001:3;
+msg = am * sin(2*pi*fm*t);
 subplot(4,1,1);
 plot(t, msg);
 title('Message Signal');
@@ -15,27 +13,37 @@ hold on;
 xlabel('Time');
 ylabel('Amplitude');
 
-carrier = ac * sin(2*pi*fc*t);
+ac = 1;
+fc = 30;
+carrier = ac * cos (2*pi*fc*t);
 subplot(4,1,2);
 plot(t, carrier);
 title('Carrier Signal');
-hold on;
-xlabel('Time');
-ylabel('Amplitude');
 
 
-modulated_signal = (ac+msg).* carrier;
+kp = 10;
+modulated_signal = ac * cos(2*pi*fc*t + kp*msg);
 subplot(4,1,3);
 plot(t, modulated_signal);
 title('Modulated Signal');
 hold on;
 xlabel('Time');
 ylabel('Amplitude');
+hold on;
+xlabel('Time');
+ylabel('Amplitude');
 
-demodulated_signal = (modulated_signal ./ carrier) - ac;
+
+x = abs(diff(modulated_signal));
+[a,b] = butter(10, 0.03);
+demodulated_signal = filter(a,b,x);
 subplot(4,1,4);
-plot(t, demodulated_signal);
+plot(demodulated_signal);
 title('Demodulated Signal');
 hold on;
 xlabel('Time');
 ylabel('Amplitude');
+
+
+
+
